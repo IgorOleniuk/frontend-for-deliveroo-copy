d216f268c2edc12be612289a91fb45770a493c57
IgorOleniuk <igor.oleniuk1@gmail.com>
2020-03-07 06:45:42 +0200

    commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="https://media.giphy.com/media/zBL9j9oiR3VM4/giphy.gif" class="uk-position-center" alt="">
+    </div>
+  </div>
+</template>
---
+++ pages/restaurants/_id.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <!--Link to go back to the previous page-->
+    <a
+      class="uk-button uk-button-primary uk-margin"
+      @click="$router.go(-1)"
+    >
+      <span uk-icon="arrow-left"></span> go back
+    </a>
+
+    <client-only>
+      <div uk-grid>
+       <!-- Left card displaying dishes-->
+        <div class="uk-width-1-3@m">
+          <div
+            v-for="dish in restaurant.dishes"
+            class="uk-margin-left"
+          >
+            <div class="uk-card uk-card-default">
+              <div class="uk-card-media-top uk-margin-auto">
+                <img :src="'http://localhost:1337/' + dish.image[0].url" alt="" />
+              </div>
+              <div class="uk-body uk-margin-left">
+                <h3 class="uk-card-title uk-align-center">{{ dish.name }} <span class="uk-badge">{{ dish.price }}$</span></h3>
+                <p>{{ dish.description }}</p>
+              </div>
+              <div class="uk-card-footer">
+                <!--Doing nothing for the moment :)-->
+                <button class="uk-button uk-button-primary" @click="addToCart(dish)">Add to cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!--Right card that will display your cart-->
+        <div class="uk-width-expand@m">
+          <Cart />
+        </div>
+      </div>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+import Cart from '~/components/Cart.vue';
+import restaurantQuery from '~/apollo/queries/restaurant/restaurant';
+
+export default {
+  components: { Cart },
+  data() {
+    return {
+      restaurant: Object,
+    }
+  },
+
+  methods: {
+    ...mapMutations({
+      addToCart: 'cart/add',
+      removeFromCart: 'cart/remove',
+    }),
+  },
+
+  apollo: {
+    restaurant: {
+      prefetch: true,
+      query: restaurantQuery,
+      variables () {
+        return { id: this.$route.params.id }
+      }
+    }
+  },
+}
+</script>
